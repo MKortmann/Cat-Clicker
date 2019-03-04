504c8c9e05d255824bc17f45cf732f6ce709e782
Dell_MVMS <companymvms@gmail.com>
2019-03-04 17:46:31 +0100

    add: another cat!

--- js/js.js
+++ js/js.js
@@ -2,11 +2,25 @@
 
 jQuery(document).ready(function($) {
 
+  let nameCat01 = "Lina";
+  let nameCat02 = "Sabrina";
+
+  $("#cat01").text(nameCat01);
+  $("#cat02").text(nameCat02);
+
   let clicks = 0;
-  $(".image")
+  let clicks02 = 0;
+  $(".imageCat01")
     .click(function() {
       clicks++;
-      $("#clicks").text(clicks);
+      $("#clicks").text(nameCat01 + " number of clicks: " + clicks);
+    })
+
+  $(".imageCat02")
+    .click(function() {
+      clicks02++;
+      $("#clicks02").text(nameCat02 + " number of clicks: " + clicks02);
+
     })
 
 });
