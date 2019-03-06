37041bcc92371e4ce3b9e7c73cdf4b8233fe875f
Dell_MVMS <companymvms@gmail.com>
2019-03-06 19:35:56 +0100

    add: add 10 cats and selection list

--- js/js.js
+++ js/js.js
@@ -2,25 +2,34 @@
 
 jQuery(document).ready(function($) {
 
-  let nameCat01 = "Lina";
-  let nameCat02 = "Sabrina";
+/**Declaring an array with the name of the cats*/
+  let nameCats = ["Lina", "Sabrina", "Odof", "Bob", "Bib",
+      "Carla", "Nata", "Flip", "Flop","Tat"];
+/**Declaring an array, filled with 0. To display the number of
+clicks of each cat.*/
+  let clicks = new Array(10).fill(0);
 
-  $("#cat01").text(nameCat01);
-  $("#cat02").text(nameCat02);
+/*Let's fill the selection box with the name of the cats*/
+$("select option")
+  .each(function(i, element) {
+    element.innerText = nameCats[i];
+  })
 
-  let clicks = 0;
-  let clicks02 = 0;
-  $(".imageCat01")
+/**If you change the select box: update image and update text*/
+  $("select")
+     .change(function() {
+       $(".image")[0].src = "./images/" + $("select")[0].value + ".jpg";
+       $(".clicks").text(nameCats[$("select")[0].selectedIndex]  + " number of clicks: " + clicks[$("select")[0].selectedIndex]);
+
+     })
+
+/*Initialization update!*/
+ $(".clicks").text(nameCats[$("select")[0].selectedIndex]  + " number of clicks: " + clicks[$("select")[0].selectedIndex]);
+
+ /*Count the clicks at specific image and display it*/
+  $(".image")
     .click(function() {
-      clicks++;
-      $("#clicks").text(nameCat01 + " number of clicks: " + clicks);
+      clicks[$("select")[0].selectedIndex]++;
+      $(".clicks").text(nameCats[$("select")[0].selectedIndex]  + " number of clicks: " + clicks[$("select")[0].selectedIndex]);
     })
-
-  $(".imageCat02")
-    .click(function() {
-      clicks02++;
-      $("#clicks02").text(nameCat02 + " number of clicks: " + clicks02);
-
-    })
-
 });
