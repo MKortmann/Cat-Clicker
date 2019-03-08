888ced52ca3b7daac0c14739d305e803478c7604
Dell_MVMS <companymvms@gmail.com>
2019-03-08 19:29:14 +0100

    refactor: MVO: code improvement

--- js/js.js
+++ js/js.js
@@ -19,9 +19,28 @@ jQuery(document).ready(function($) {
       })
     },
 
+    updateSelectionBox: function(selectionBox) {
+      /**If you change the select box: update image and update text*/
+        selectionBox
+           .change(function() {
+             $(".image")[0].src = "./images/" + $("select")[0].value + ".jpg";
+             $(".clicks").text(model.nameCats[$("select")[0].selectedIndex]  + " number of clicks: " + model.clicks[$("select")[0].selectedIndex]);
+
+           })
+    },
+
     updateText: function(clicks, selection) {
       clicks.text(model.nameCats[selection.selectedIndex]  + " number of clicks: "
         + model.clicks[selection.selectedIndex]);
+    },
+
+    clicks: function(image, selection) {
+
+        model.clicks[selection.selectedIndex]++;
+        $(".image")[0].src = "./images/" + $("select")[0].value + ".jpg";
+        $(".clicks").text(model.nameCats[$("select")[0].selectedIndex]  + " number of clicks: " + model.clicks[$("select")[0].selectedIndex]);
+        // $(".clicks").text(model.nameCats[$("select")[0].selectedIndex]  + " number of clicks: " + model.clicks[$("select")[0].selectedIndex]);
+
     }
   }
 
@@ -33,30 +52,30 @@ jQuery(document).ready(function($) {
     octopus.addElementList(selection);
   },
 
+  changeSelectionBox: function() {
+    let selectionBox = $("select");
+    octopus.updateSelectionBox(selectionBox);
+  },
+
   render: function() {
     /*Initialization update!*/
     let clicks = $(".clicks");
     let selection = $("select")[0];
     octopus.updateText(clicks, selection);
+  },
 
+  clicks: function() {
+    /*Count the clicks at specific image and display it*/
+     let image = $(".image");
+     let selection = $("select")[0];
+     octopus.clicks(image, selection);
   }
 };
 
 view.initSelectionBox();
-/**If you change the select box: update image and update text*/
-  $("select")
-     .change(function() {
-       $(".image")[0].src = "./images/" + $("select")[0].value + ".jpg";
-       $(".clicks").text(model.nameCats[$("select")[0].selectedIndex]  + " number of clicks: " + model.clicks[$("select")[0].selectedIndex]);
-
-     })
+view.render();
+$("select").click(view.changeSelectionBox);
+$(".image").click(view.clicks);
 
 
-
- /*Count the clicks at specific image and display it*/
-  $(".image")
-    .click(function() {
-      model.clicks[$("select")[0].selectedIndex]++;
-      $(".clicks").text(model.nameCats[$("select")[0].selectedIndex]  + " number of clicks: " + model.clicks[$("select")[0].selectedIndex]);
-    })
 });
