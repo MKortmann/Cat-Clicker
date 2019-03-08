345df5a9184053627471c2dd7a1cab5223a06c97
Dell_MVMS <companymvms@gmail.com>
2019-03-08 18:43:22 +0100

    refactor: MVO, adding octopus

--- js/js.js
+++ js/js.js
@@ -11,18 +11,38 @@ jQuery(document).ready(function($) {
     clicks: new Array(10).fill(0)
   };
 
+  let octopus = {
+    addElementList: function(selection) {
+      selection
+      .each(function(i, element) {
+        element.innerText = model.nameCats[i];
+      })
+    },
+
+    updateText: function(clicks, selection) {
+      clicks.text(model.nameCats[selection.selectedIndex]  + " number of clicks: "
+        + model.clicks[selection.selectedIndex]);
+    }
+  }
+
 /*Let's fill the selection box with the name of the cats*/
   let view = {
 
-  init: function() {
-  $("select option")
-  .each(function(i, element) {
-    element.innerText = model.nameCats[i];
-  })
+  initSelectionBox: function() {
+    let selection = $("select option");
+    octopus.addElementList(selection);
+  },
+
+  render: function() {
+    /*Initialization update!*/
+    let clicks = $(".clicks");
+    let selection = $("select")[0];
+    octopus.updateText(clicks, selection);
+
   }
 };
 
-view.init();
+view.initSelectionBox();
 /**If you change the select box: update image and update text*/
   $("select")
      .change(function() {
@@ -31,8 +51,7 @@ view.init();
 
      })
 
-/*Initialization update!*/
- $(".clicks").text(model.nameCats[$("select")[0].selectedIndex]  + " number of clicks: " + model.clicks[$("select")[0].selectedIndex]);
+
 
  /*Count the clicks at specific image and display it*/
   $(".image")
